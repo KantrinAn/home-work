bba82a4e137adc0c6656c37fc3c7283e4b643c24
KatrinAn <anpipinakaka@gmail.com>
2022-12-13 23:36:34 +0200

    add ignore

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        random.nextInt();
+        int i = 0;
+        if (i <= 10) {
+            int num = random.nextInt(10);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Вкажіть число від 0 до 10");
+            int first = scanner.nextInt();
+            if (first == num) {
+                System.out.println("Ви вгадали");
+            } else if (num != first) {
+                System.out.println("Спробуйте ще раз");
+            }
+
+            System.out.println("Вкажіть друге число");
+            int second = scanner.nextInt();
+            if (second == num) {
+                System.out.println("Ви вгадали");
+            } else if (second != num) {
+                System.out.println("Спробуйте ще раз");
+            }
+
+            System.out.println("Вкажіть третє число");
+            int third = scanner.nextInt();
+            if (third == num) {
+                System.out.println("Ви вгадали");
+            } else if (third != num) {
+                System.out.println("Ви програли");
+            }
+
+            System.out.println("Було загадане число " + num);
+        }
+
+    }
+}
