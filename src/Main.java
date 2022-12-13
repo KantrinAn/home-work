9c086f91128ec57d6a85a2703409876b9d14e1c8
KatrinAn <anpipinakaka@gmail.com>
2022-12-13 23:59:22 +0200

    add for

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Random random = new Random();
         random.nextInt();
-        int i = 0;
-        if (i <= 10) {
+        for (int i = 0; i <= 10; i++) {
             int num = random.nextInt(10);
             Scanner scanner = new Scanner(System.in);
             System.out.println("Вкажіть число від 0 до 10");
             int first = scanner.nextInt();
             if (first == num) {
                 System.out.println("Ви вгадали");
-            } else if (num != first) {
+            }
+            else if (num != first) {
                 System.out.println("Спробуйте ще раз");
             }
-
             System.out.println("Вкажіть друге число");
             int second = scanner.nextInt();
             if (second == num) {
@@ -24,17 +23,14 @@ public class Main {
             } else if (second != num) {
                 System.out.println("Спробуйте ще раз");
             }
-
             System.out.println("Вкажіть третє число");
             int third = scanner.nextInt();
-            if (third == num) {
+            if (third == num){
                 System.out.println("Ви вгадали");
             } else if (third != num) {
                 System.out.println("Ви програли");
             }
-
             System.out.println("Було загадане число " + num);
-        }
-
+            break;}
     }
 }
